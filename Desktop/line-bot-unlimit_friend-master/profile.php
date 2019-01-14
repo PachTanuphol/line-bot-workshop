a3a3be0d5f4fcf58084dcf733590f3a64c24084d
PachTanuphol <46663599+PachTanuphol@users.noreply.github.com>
2019-01-14 11:27:44 +0700

    update value

--- Desktop/line-bot-unlimit_friend-master/profile.php
+++ Desktop/line-bot-unlimit_friend-master/profile.php
@@ -1,9 +1,9 @@
 <?php
 
 
-$access_token = '*Channel_Access_Token*';
+$access_token = 'jvW9HuIGNPGjk0TGVxwwKBeF2aX80LA9jCym2oeAAvYiWDrdPfbGyhbKJnhwpQNjxJ+iJJr05nQJ1o6equbA+Y1hJzHczbv1vFkBkJrgXFyoaLTYlYkZQNlWZ3AxIW3Q4YmqL9CES5B5FyTuWtA9mgdB04t89/1O/w1cDnyilFU=';
 
-$userId = '*User_ID*';
+$userId = 'U8a5e108504e65cb116676451aa18b961';
 
 $url = 'https://api.line.me/v2/bot/profile/'.$userId;
 
